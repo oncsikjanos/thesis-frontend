05801a94541c4b5c771966f4f73b8525c32fc2c9
Oncsik János <janoka82@gmail.com>
2024-10-11 12:57:35 +0200

    add register format

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerform: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.registerform = this.formBuilder.group({
+      fullName: this.formBuilder.group({
+        surname: [''],
+        foreName:['']
+      }),
+      email : ['', Validators.required],
+      password: ['', Validators.required],
+      passwordConfirm: ['', Validators.required]
+    });
+  }
 }
